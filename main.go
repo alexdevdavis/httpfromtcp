bdff6e93b28d0aa5ebf4d8163c48d73dd458abc9
alexdevdavis <alexdevdavis@gmail.com>
2025-04-07 19:15:32 +0100

    refactor: close file reader resource

--- main.go
+++ main.go
@@ -7,25 +7,27 @@ import (
 	"strings"
 )
 
+const inputFilePath = "messages.txt"
+
 func main() {
 	f, err := os.Open("messages.txt")
+	defer f.Close()
 	if err != nil {
-		fmt.Printf("something went wrong reading file: %v", err)
+		fmt.Printf("something went wrong reading file %s: %v", inputFilePath, err)
 		return
 	}
-	byteStream := make([]byte, 8, 8)
+	buffer := make([]byte, 8, 8)
 	currentLine := ""
 	for {
-		_, err := f.Read(byteStream)
+		_, err := f.Read(buffer)
 		if err == io.EOF {
 			return
 		}
-		parts := strings.Split(string(byteStream), "\n")
+		parts := strings.Split(string(buffer), "\n")
 		currentLine += parts[0]
 		if len(parts) > 1 {
 			fmt.Printf("read: %s\n", currentLine)
 			currentLine = parts[1]
 		}
-
 	}
 }
