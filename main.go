7268b5416511a36338c30813623a759ae88c2806
alexdevdavis <alexdevdavis@gmail.com>
2025-04-07 19:39:44 +0100

    refactor: use channel to perform read operation

--- main.go
+++ main.go
@@ -11,23 +11,40 @@ const inputFilePath = "messages.txt"
 
 func main() {
 	f, err := os.Open("messages.txt")
-	defer f.Close()
+
 	if err != nil {
 		fmt.Printf("something went wrong reading file %s: %v", inputFilePath, err)
 		return
 	}
-	buffer := make([]byte, 8, 8)
-	currentLine := ""
-	for {
-		_, err := f.Read(buffer)
-		if err == io.EOF {
-			return
-		}
-		parts := strings.Split(string(buffer), "\n")
-		currentLine += parts[0]
-		if len(parts) > 1 {
-			fmt.Printf("read: %s\n", currentLine)
-			currentLine = parts[1]
-		}
+	linesChan := getLinesChannel(f)
+
+	for line := range linesChan {
+		fmt.Printf("read: %s\n", line)
 	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+
+	go func() {
+		defer close(ch)
+		defer f.Close()
+		buffer := make([]byte, 8, 8)
+		currentLine := ""
+		for {
+			_, err := f.Read(buffer)
+
+			if err == io.EOF {
+				return
+			}
+			parts := strings.Split(string(buffer), "\n")
+			currentLine += parts[0]
+			if len(parts) > 1 {
+				ch <- currentLine
+				currentLine = parts[len(parts)-1]
+			}
+
+		}
+	}()
+	return ch
 }
